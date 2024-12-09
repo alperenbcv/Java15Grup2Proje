06d725d9b6820033c6127e714ce1a51c72832252
alperenbcv <alperenbcv@gmail.com>
2024-12-09 10:19:46 +0300

    Initial commit

---
+++ src/test/java/org/example/java15grup2proje/Java15Grup2ProjeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.java15grup2proje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java15Grup2ProjeApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
