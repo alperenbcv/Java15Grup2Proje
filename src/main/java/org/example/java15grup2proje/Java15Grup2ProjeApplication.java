8287f4ce68fa0ae85b253c943b7a32880c1e80ce
Taynis910 <mehmet_tufan910@hotmail.com>
2024-12-13 14:00:10 +0300

    deneme

--- src/main/java/org/example/java15grup2proje/Java15Grup2ProjeApplication.java
+++ src/main/java/org/example/java15grup2proje/Java15Grup2ProjeApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//test
 public class Java15Grup2ProjeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Java15Grup2ProjeApplication.class, args);
